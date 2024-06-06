21f391227a8b694a9f4abfa4e0bf1afe2ab415d4
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-06-06 11:17:28 +0600

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from "react";
+import Productx from "./Productx";
 
 function App() {
   return (
-    <div>App</div>
+    <div className="w-full h-screen bg-zinc-900 p-4">
+      <div className="w-44 h-32 rounded-xl bg-red-600 p-3 text-white">
+      </div>
+      <Productx/>
+    </div>
   )
 }
 
